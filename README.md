9e0f3f3f25da13cf0daaaae094604312799a8e75
Steve Winton <stevewinton@gmail.com>
2020-08-25 19:53:12 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# `github-apps-hello-world`
+> :wave: :earth_americas: The _Hello World_ of GitHub Apps.
+[![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
+
+## About
+
+This is the _Hello World_ of GitHub Apps.
+
+## Usage
+
+```shell
+npx github-apps-hello-world | pino-pretty
+```
---
+++ github-apps-hello-world.js
@@ -0,0 +1,34 @@
+const logger = require('pino')();
+
+const { Octokit } = require('@octokit/core');
+const { createAppAuth } = require('@octokit/auth-app');
+
+const appCredentials = require('./lib/app-credentials');
+
+async function main() {
+  try {
+    // Instantiate new Octokit client
+    const octokit = new Octokit({
+      baseUrl: `https://${process.env.GHE_HOST}/api/v3`,
+      authStrategy: createAppAuth,
+      auth: {
+        ...appCredentials
+      }
+    });
+
+    // Create issue in demo-days/Spoon-Knife
+    // https://docs.github.com/en/rest/reference/issues#create-an-issue
+    const issue = await octokit.request('POST /repos/:owner/:repo/issues', {
+      owner: 'demo-days',
+      repo: 'Spoon-Knife',
+      title: 'Hello world',
+      body: ':wave: :earth_americas:'
+    });
+    logger.info(issue);
+  } catch (e) {
+    logger.error(e);
+    process.exit(1);
+  }
+}
+
+main();
